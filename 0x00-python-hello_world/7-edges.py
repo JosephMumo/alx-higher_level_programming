9c4a07f242cdbaf5c612145e36a390d6eaa562da
JosephMumo <josephmumo25@gmail.com>
2022-08-22 15:45:51 -0700

    scripting

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[7:]
-middle_word = word[1:6]
+middle_word = word[1:8]
 print(f"First 3 letters: {}".format(word_first_3))
 print(f"Last 2 letters: {}".format(word_last_2))
 print(f"Middle word: {}".format(middle_word))
