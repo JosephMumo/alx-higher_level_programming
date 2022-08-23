689824a44668358a311ff7a06551b3826bcf277e
JosephMumo <josephmumo25@gmail.com>
2022-08-23 12:24:59 -0700

    scripting

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for i in range(101):
+        if i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+    print(f"{i} ")
